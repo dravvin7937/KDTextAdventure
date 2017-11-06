42207baa9051ed4257534211f6b3fe7ea4561128
Katherin <ksolis3197@bths.edu>
2017-11-05 22:14:37 -0500

    personalities

--- TextBasedAdventure/src/game/GameRunner.java
+++ TextBasedAdventure/src/game/GameRunner.java
@@ -6,6 +6,7 @@ import ingredients.Ingredient;
 public class GameRunner {
 	
 	private static String[] ingredients = {"eggs", "milk", "pasta", "cheese", "apple", "pumpkin", "sugar", "flour", "butter", "cinnamon"};
+	private static String[] associatedRecipes = {"eggnog", "pancakes", "pie", "mac & cheese"};
 	public static void main(String[] args)
 	{
 		System.out.println("------------");
@@ -13,13 +14,13 @@ public class GameRunner {
 		System.out.println("Navigate the floors in order to find the missing ingredient ");
 		System.out.println("Inspired by Master Chef and Chopped");
 		System.out.println("------------");
-		System.out.println("Your Recipe: Mac and Cheese");
+		System.out.println("Your Recipe: "+ GameRunner.getRandom(associatedRecipes));
 		System.out.println("Missing ingredient: " + Ingredient.getRandom(ingredients));
 		
 		System.out.println("------------");
 		Board.printBoard(5);
 		System.out.println("------------");
-		System.out.println(" Your Current Position:");
+		//System.out.println(" Your Current Position:");
 	}
 	
 }
--- TextBasedAdventure/src/people/Person.java
+++ TextBasedAdventure/src/people/Person.java
@@ -10,6 +10,12 @@ public class Person extends item{
 	
 	static String[] chefs = {"Gordon Ramsey", "Bobby Flay", "Giada DiLaurentis", "Rachel Ray", "Karys Logue", "Gordon Ramsey", "Bobby Flay", "Giada DiLaurentis", "Rachel Ray", "Karys Logue"}; 
 	
+	static String gRamsey = " Let pancake batter rest, covered, for 15 minutes so it can thicken and create fluffier pancakes. Eggs are essential to a good pancake";
+	static String bFlay = "I like to do my scramble with a little bit of scallions and romesco, which is a peppery, Spanish, pesto-like sauce";
+	static String gDilaur = "I like to add lots of different cheeses, and sometimes I add prosciutto, sometimes I add salami — it just depends.";
+	static String rRay = "Apples and Pumpkins make great pies due to their texture";
+	static String kLogue = "Everything will taste richer and better with butter";
+	
 	public Person(String dialogue)
 	{
 		super(name,description);
